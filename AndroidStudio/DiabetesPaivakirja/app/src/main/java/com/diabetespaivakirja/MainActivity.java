646eb965079a90aef75b104448d02180e8544dca
joelrau <71751328+j0elr4u@users.noreply.github.com>
2020-09-28 19:36:25 +0300

    Updated MainActivity
    Päivittää nimen ruudulla.
    Päivitetty kalendari hommii.

--- AndroidStudio/DiabetesPaivakirja/app/src/main/java/com/diabetespaivakirja/MainActivity.java
+++ AndroidStudio/DiabetesPaivakirja/app/src/main/java/com/diabetespaivakirja/MainActivity.java
@@ -8,12 +8,16 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.CalendarView;
-import android.widget.DatePicker;
 import android.widget.TextView;
 
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+    private static int date;
+    private static int month;
+    private static int year;
+
+    private SharedPrefs sp = new SharedPrefs("prefs");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,24 +25,52 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         calendarStuff();
+        updateUI();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        updateUI();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(!isCalendarVisible()) {
+            super.onBackPressed();
+        } else {
+            toggleCalendar();
+        }
+    }
+
+    private void updateUI() {
+        updateName();
+    }
+
+    private void updateName() {
+        final TextView name_view = findViewById(R.id.nameView);
+        String name = this.sp.getDefaultPrefString("first_name", this) + " " + this.sp.getDefaultPrefString("last_name", this);
+
+        name_view.setText(name);
+    }
+
+    // Calendar stuff
     private void calendarStuff() {
-        final CalendarView calender = findViewById(R.id.calendarView);
+        final CalendarView calendar_view = getCalendarView();
         final TextView date_view = findViewById(R.id.dateView);
         final Calendar calendar = Calendar.getInstance();
-
-        int date = calendar.get(Calendar.DATE);
-        int month = calendar.get(Calendar.MONTH) + 1;
-        int year = calendar.get(Calendar.YEAR);
-        String ddMmYy = date + "/" + month + "/" + year;
+        MainActivity.date = calendar.get(Calendar.DATE);
+        MainActivity.month = calendar.get(Calendar.MONTH) + 1;
+        MainActivity.year = calendar.get(Calendar.YEAR);
+        String ddMmYy = MainActivity.date + "/" + MainActivity.month + "/" + MainActivity.year;
         date_view.setText(ddMmYy);
 
-        calender.setVisibility(View.GONE);
+        toggleCalendar();
 
         // Kopioitu täältä: https://www.geeksforgeeks.org/android-creating-a-calendar-view-app/
         // Add Listener in calendar
-        calender
+        calendar_view
                 .setOnDateChangeListener(
                         new CalendarView
                                 .OnDateChangeListener() {
@@ -58,24 +90,47 @@ public class MainActivity extends AppCompatActivity {
                                 // format in String type Variable
                                 // Add 1 in month because month
                                 // index is start with 0
+                                MainActivity.date = dayOfMonth;
+                                MainActivity.month = (month + 1);
+                                MainActivity.year = year;
                                 String Date
-                                        = dayOfMonth + "/"
-                                        + (month + 1) + "/" + year;
+                                        = MainActivity.date + "/"
+                                        + MainActivity.month + "/" + MainActivity.year;
 
                                 // set this date in TextView for Display
                                 date_view.setText(Date);
-                                calender.setVisibility(View.GONE);
+                                toggleCalendar();
                             }
                         });
     }
 
-    public void onDateViewPressed(View view) {
-        final CalendarView calendarView = findViewById(R.id.calendarView);
-        if(calendarView.getVisibility() == View.GONE) {
-            calendarView.setVisibility(View.VISIBLE);
-        } else {
-            calendarView.setVisibility(View.GONE);
+    private void toggleCalendar() {
+        final CalendarView calendar_view = getCalendarView();
+        if(isCalendarVisible()) {
+            calendar_view.setVisibility(View.GONE);
         }
+        else {
+            calendar_view.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private boolean isCalendarVisible() {
+        final CalendarView calendar_view = getCalendarView();
+        if(calendar_view.getVisibility() == View.VISIBLE) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private CalendarView getCalendarView() {
+        return findViewById(R.id.calendarView);
+    }
+
+    // Buttons
+
+    public void onDateViewPressed(View view) {
+        toggleCalendar();
     }
 
     public void onButtonPressed_Settings(View view){
