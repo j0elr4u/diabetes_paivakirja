42a31fdb7ac0db292ee8420cd7e690d51b77a31d
Sasha Wouse <snowpill@hotmail.fi>
2020-09-29 10:32:19 +0300

    Lisäsin AnyChart-Android kirjasto. Linkit löytyy DP Refernces dokumintaatiossa. Mutta koodia tarvitse muuttaa meidän sovelluksen varten.

--- AndroidStudio/DiabetesPaivakirja/app/src/main/java/com/diabetespaivakirja/ArvoActivity.java
+++ AndroidStudio/DiabetesPaivakirja/app/src/main/java/com/diabetespaivakirja/ArvoActivity.java
@@ -1,8 +1,24 @@
 package com.diabetespaivakirja;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Cartesian;
+import com.anychart.core.cartesian.series.Column;
+import com.anychart.enums.Anchor;
+import com.anychart.enums.HoverMode;
+import com.anychart.enums.Position;
+import com.anychart.enums.TooltipPositionMode;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class ArvoActivity extends AppCompatActivity {
 
@@ -10,6 +26,42 @@ public class ArvoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_arvo);
+
+        AnyChartView anyChartView = findViewById(R.id.any_chart_view);
+        //anyChartView.setProgressBar(findViewById(R.id.progress_bar));
+
+        Cartesian cartesian = AnyChart.column();
+
+        List<DataEntry> data = new ArrayList<>();
+        data.add(new ValueDataEntry("ma", 7));
+        data.add(new ValueDataEntry("ti", 8));
+        data.add(new ValueDataEntry("kv", 10));
+
+
+        Column column = cartesian.column(data);
+
+        column.tooltip()
+                .titleFormat("{%X}")
+                .position(Position.CENTER_BOTTOM)
+                .anchor(Anchor.CENTER_BOTTOM)
+                .offsetX(0d)
+                .offsetY(5d)
+                .format("mmol/l{%Value}{groupsSeparator: }");
+
+        cartesian.animation(true);
+        cartesian.title("Verensokeriarvo");
+
+        cartesian.yScale().minimum(0d);
+
+        cartesian.yAxis(0).labels().format("mmol/l{%Value}{groupsSeparator: }");
+
+        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
+        cartesian.interactivity().hoverMode(HoverMode.BY_X);
+
+        cartesian.xAxis(0).title("Pvm");
+        cartesian.yAxis(0).title("Keskiarvo");
+
+        anyChartView.setChart(cartesian);
 
     }
 
